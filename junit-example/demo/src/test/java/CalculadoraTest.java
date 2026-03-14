cdb05b321aaf4ae45e0f2653d480814ee5d1ee68
Hallason Matias <122497324+Hallason823@users.noreply.github.com>
2026-03-14 13:48:26 +0000

    fix: add junit-example as normal folder instead of submodule

---
+++ junit-example/demo/src/test/java/CalculadoraTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import com.example.Calculadora;
+
+class CalculadoraTest {
+    @Test
+    public void somaTest() {
+        Calculadora c = new Calculadora();
+
+    }
+
+    
+}
