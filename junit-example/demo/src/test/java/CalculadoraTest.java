55a72cab2d0bf3e6ede73f57e23d3aa94b175180
Hallason Matias <122497324+Hallason823@users.noreply.github.com>
2026-03-14 14:45:17 +0000

    feat: add the calculator test

--- junit-example/demo/src/test/java/CalculadoraTest.java
+++ junit-example/demo/src/test/java/CalculadoraTest.java
@@ -1,13 +1,49 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.example.Calculadora;
 
 class CalculadoraTest {
+
+    int VALUES[] = {3, 5};
+    Calculadora c;
+
+    @BeforeEach
+    public void setup() {
+        this.c = new Calculadora();
+    } 
+
     @Test
     public void somaTest() {
-        Calculadora c = new Calculadora();
-
+        assertEquals(8, c.somar(VALUES[0], VALUES[1]));        
     }
 
-    
+    @Test
+    public void subTest() {
+        assertEquals(-2, c.subtrair(VALUES[0], VALUES[1]));
+    }
+
+    @Test
+    public void multTest() {
+        assertEquals(15, c.multiplicar(VALUES[0], VALUES[1]));
+    }
+
+    @Test
+    public void divTest() {
+        assumeTrue(VALUES[1] != 0);
+        assertEquals(0.6, c.dividir(VALUES[0], VALUES[1]));
+    }
+
+    @Test
+    public void divZeroTest() {
+        int specialValues[] = {4, 0};
+        assumeTrue(specialValues[1] == 0);
+        assertThrows(IllegalArgumentException.class, () -> {
+            c.dividir(specialValues[0], specialValues[1]);
+        });
+    }
 }
